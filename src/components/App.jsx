13aadd73cc79b2c2cd65e579e9a4db45724681a4
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2025-11-29 11:36:16 +0000

    Simple State Access & Conditional Rendering.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,25 @@
+import Header from "./Bootstrap/Header";
+import ContainerRow from "./Bootstrap/ContainerRow";
+import { useState } from "react";
+
 const App = () => {
+  const [darkMode, setDarkMode] = useState(true);
+  const handleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  }
   return <main>
-    <h1>SIT Workshop</h1>
-    <p>Hello Everyone!</p>
+    <Header dark={darkMode} className="justify-content-center">SIT Workshop</Header>
+    <ContainerRow>
+      <div className="col-12 my-4">
+        <button className="btn btn-primary" onClick={handleMode}>{darkMode ? 'Light' : 'Dark'} Mode</button>
+      </div>
+      <div className="col-12 my-4">
+        <pre className="border rounded bg-light p-2">
+          {JSON.stringify({ darkMode }, null, 2)}
+        </pre>
+      </div>
+    </ContainerRow>
   </main>;
 };
 
