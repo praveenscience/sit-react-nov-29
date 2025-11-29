a3827b16fd495aa3ba769b38ceffbc38f97b8333
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2025-11-29 11:41:37 +0000

    Controlled Components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,39 @@ import { useState } from "react";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
+  const [myName, setMyName] = useState("");
+
   const handleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   }
-  return <main>
-    <Header dark={darkMode} className="justify-content-center">SIT Workshop</Header>
-    <ContainerRow>
-      <div className="col-12 my-4">
-        <button className="btn btn-primary" onClick={handleMode}>{darkMode ? 'Light' : 'Dark'} Mode</button>
-      </div>
-      <div className="col-12 my-4">
-        <pre className="border rounded bg-light p-2">
-          {JSON.stringify({ darkMode }, null, 2)}
-        </pre>
-      </div>
-    </ContainerRow>
-  </main>;
+
+  const handleChangeName = e => {
+    setMyName(e.target.value);
+  }
+
+  return (
+    <main>
+      <Header dark={darkMode} className="justify-content-center">SIT Workshop</Header>
+      <ContainerRow>
+        <div className="col-12 my-4">
+          <button className="btn btn-primary" onClick={handleMode}>{darkMode ? 'Light' : 'Dark'} Mode</button>
+        </div>
+        <div className="col-12 mt-4 mb-2">
+          <p>Controlled Components</p>
+          <input type="text" value={myName} name="text-1" onChange={handleChangeName} />
+        </div>
+        <div className="col-12">
+          <input type="text" value={myName} name="text-2" onChange={handleChangeName} />
+        </div>
+        <div className="col-12 my-4">
+          <pre className="border rounded bg-light p-2">
+            {JSON.stringify({ darkMode, myName }, null, 2)}
+          </pre>
+        </div>
+      </ContainerRow>
+    </main>
+  );
 };
 
 export default App;
