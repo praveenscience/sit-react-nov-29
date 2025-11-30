a63e3fc405df53e16989201ccc0bcaa9959e9c6a
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2025-11-30 11:54:42 +0000

    Add some dummy users and a delay.

--- server/users.js
+++ server/users.js
@@ -3,9 +3,30 @@ import express from "express";
 // Create a new Route.
 const app = express.Router();
 // Create a list of users.
-const users = [];
+const users = [
+  {
+    ID: "praveenscience",
+    Name: "Praveen Kumar Purushothaman",
+    Avatar: "https://github.com/praveenscience.png"
+  },
+  {
+    ID: "rutujavatane27-lab",
+    Name: "Rutuja Vatane",
+    Avatar: "https://github.com/rutujavatane27-lab"
+  },
+  {
+    ID: "sohelkhan006",
+    Name: "Sohel Khan",
+    Avatar: "https://github.com/sohelkhan006"
+  }
+];
 // Define some routes.
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 500);
+  });
   res.json(users);
 });
 // Export the file.
