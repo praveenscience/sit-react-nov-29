d590f5a227cc66776fd2e1c3d862cb95cf0199f9
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2025-11-30 11:54:56 +0000

    Integrate backend inside Vite.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: "./",
+  server: {
+    cors: {
+      origin: "http://localhost:5174/"
+    },
+    proxy: {
+      "/api": "http://localhost:5174"
+    }
+  }
 });
