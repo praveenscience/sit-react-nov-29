8789334286089a98c3d3ad26f5b9cbfabf5f25c4
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2025-11-30 10:24:33 +0000

    Updated with Prettier.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,28 +6,42 @@ const App = () => {
   const [darkMode, setDarkMode] = useState(true);
   const [myName, setMyName] = useState("");
 
-  const handleMode = e => {
+  const handleMode = (e) => {
     e.preventDefault();
     setDarkMode(!darkMode);
-  }
+  };
 
-  const handleChangeName = e => {
+  const handleChangeName = (e) => {
     setMyName(e.target.value);
-  }
+  };
 
   return (
     <main>
-      <Header dark={darkMode} className="justify-content-center">SIT Workshop <small>({import.meta.env.MODE})</small></Header>
+      <Header dark={darkMode} className="justify-content-center">
+        SIT Workshop <small>({import.meta.env.MODE})</small>
+      </Header>
       <ContainerRow>
         <div className="col-12 my-4">
-          <button className="btn btn-primary" onClick={handleMode}>{darkMode ? 'Light' : 'Dark'} Mode</button>
+          <button className="btn btn-primary" onClick={handleMode}>
+            {darkMode ? "Light" : "Dark"} Mode
+          </button>
         </div>
         <div className="col-12 mt-4 mb-2">
           <p>Controlled Components</p>
-          <input type="text" value={myName} name="text-1" onChange={handleChangeName} />
+          <input
+            type="text"
+            value={myName}
+            name="text-1"
+            onChange={handleChangeName}
+          />
         </div>
         <div className="col-12">
-          <input type="text" value={myName} name="text-2" onChange={handleChangeName} />
+          <input
+            type="text"
+            value={myName}
+            name="text-2"
+            onChange={handleChangeName}
+          />
         </div>
         <div className="col-12 my-4">
           <pre className="border rounded bg-light p-2">
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './components/App'
+import App from "./components/App";
 
-import './styles/index.scss'
+import "./styles/index.scss";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
